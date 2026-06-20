767ab4444ecbc98ae263452312bf1fadfa0e9418
Tri <trixinchao@gmail.com>
2026-06-20 20:01:54 +0700

    fix: hỗ trợ parse link youtube shorts

--- src/pages/admin.js
+++ src/pages/admin.js
@@ -656,7 +656,7 @@ function setupUpload() {
       const url = ytInput.value.trim();
       if (!url) return;
       
-      const regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
+      const regExp = /^.*(youtu\.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=|shorts\/)([^#\&\?]*).*/;
       const match = url.match(regExp);
       const videoId = (match && match[2].length === 11) ? match[2] : null;
 
